2d0e10ed5155bf07df8ca4df4fcac33bbe8662fa
yarvod <yar.vod.ole@gmail.com>
2022-12-24 03:53:36 +0300

    fix

--- bot_backend/entrypoint.sh
+++ bot_backend/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$1" = "run_django" ]; then
   python manage.py migrate
   python manage.py collectstatic --no-input
-  exec gunicorn backend.wsgi:application -b 0.0.0.0:8000 --reload
+  exec gunicorn bot_backend.wsgi:application -b 0.0.0.0:8000 --reload
 fi
 
 if [ "$1" = 'run_bot' ]; then
